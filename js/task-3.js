68cd4e537320e4d8a6b8f3c0d34382696fbc467a
Marina <marina.slepinina3@gmail.com>
2025-01-30 21:07:06 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,18 @@
-function getElementWidth(content, padding, border) {
-    border = border.replace(",", ".");
-    padding = padding.replace(",", ".");
-    content = content.replace(",", ".");
-    content = Number.parseFloat(content);
-    const totalPadding = Number.parseFloat(padding) * 2;
-    const totalBorder = Number.parseFloat(border) * 2;
-    return (content + totalPadding + totalBorder);
+function checkForSpam(message) {
+    message = message.toLowerCase();
+    console.log(message);
+    if (message.includes("spam") || message.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
